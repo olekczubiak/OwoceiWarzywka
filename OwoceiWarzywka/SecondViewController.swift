ee238e11419207707429ece37fa7859bc66b3288
olekczubiak <olekczubiak@protonmail.com>
2020-01-24 11:12:37 +0100

    przed kraszami backup

--- OwoceiWarzywka/SecondViewController.swift
+++ OwoceiWarzywka/SecondViewController.swift
@@ -8,35 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController,UITableViewDelegate ,UITableViewDataSource{
+class SecondViewController: UIViewController{
     
-    var tasks: [Task] = []
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell",for: indexPath) as! TaskCell
-        
-        cell.taskNameLabel.text = tasks[indexPath.row].name
-        
-      
-        
-        return cell
-    }
-    override func viewDidLoad() {
-        tasks.append(Task(name: "banan"))
-        
-    }
-}
-
-class Task {
-    var name = ""
-    var checked = false
-    
-    convenience init (name: String){
-    self.init()
-    self.name = name
-}
+   
 }
